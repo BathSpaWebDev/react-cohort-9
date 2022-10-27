cf7fb9f8eaf203155201f7ec9689b6011f32164b
OlgitH <berman.oj@gmail.com>
2022-10-27 12:00:46 +0100

    add form component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Box from './components/Box.js'
+import Box from './components/Box'; //imports the Box.js 
+import Form from './components/Form'; //imports the index.js from the Form folder
 
 function App() {
   return (
     <div className="App">
           <h1>Cohort 9 work</h1>
-          <Box>This is the first box</Box>
+
+          {/* <Box>This is the first box</Box> */}
+
+          <Form />
+
     </div>
   );
 }
